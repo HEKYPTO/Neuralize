b14d8f4f4df456bfe2ca8657ae172e1f7d4a8a30
HEKPYTO <tsunyanapat@gmail.com>
2024-08-08 20:23:23 +0700

    WOOT

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,26 +14,26 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-//        int[] layerSizes = {784, 241, 88, 10};
-//        Function[] activationFunctions = {Function.TANH, Function.TANH, Function.TANH};
+//       int[] layerSizes = {784, 241, 88, 10};
+//       Function[] activationFunctions = {Function.TANH, Function.TANH, Function.TANH};
 //
-//        Data[] data = DataReader.loader("mnist-train-processed.txt"); // Data so large, ask me for Dataset, I'll give it to you !
+//       Data[] data = DataReader.loader("mnist-train-processed.txt"); // Data so large, ask me for Dataset, I'll give it to you !
 //
-//        Network network = new Network(layerSizes, activationFunctions, data);
+//       Network network = new Network(layerSizes, activationFunctions, data);
 //
-//        Network.train(20, 0.000761); // Takes around 5 mins to train, too long to train in test room !
+//       Network.train(20, 0.000761); // Takes around 5 mins to train, too long to train in test room !
 //
-//        network.save("saved_weights.txt");
+//       network.save("src/saved_weights.txt");
 
-        Network.load("saved_weights.txt");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Input filename from sample folder: ");
-        String line = scanner.nextLine().trim();
-        double[] sample = ImageProcessor.process("src/sample/" + line + ".jpg");
-        double[] result = Network.predict(sample);
-        int index = findIndexOfMax(result);
+         Network.load("src/saved_weights.txt");
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Input filename from sample folder: ");
+         String line = scanner.nextLine().trim();
+         double[] sample = ImageProcessor.process("src/sample/" + line + ".jpg");
+         double[] result = Network.predict(sample);
+         int index = findIndexOfMax(result);
 
-        System.out.printf("The prediction is: %d, Confidence: %.6f%n", index, result[index]);
+         System.out.printf("The prediction is: %d, Confidence: %.6f%n", index, result[index]);
     }
 
     public static int findIndexOfMax(double[] values) {
